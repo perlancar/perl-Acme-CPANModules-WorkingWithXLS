0be29fe9ca3c6a5f2ade3532e0a763fb5c8e9c23
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-20 13:25:46 +0700

    [ux] Add alias: Spreadsheet

---
+++ lib/Acme/CPANModules/Spreadsheet.pm
@@ -0,0 +1,5 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Acme::CPANModules::Spreadsheet;
+use alias::module 'Acme::CPANModules::WorkingWithXLS';
+1;
+# ABSTRACT: Currently alias for Acme::CPANModules::WorkingWithXLS
