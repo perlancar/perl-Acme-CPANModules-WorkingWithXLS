85b44967f116ee4c2bd1eff1d7471d45d2782779
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-04 13:43:15 +0700

    Add aliases: WorkingWith{ODS,XLSX}

---
+++ lib/Acme/CPANModules/WorkingWithODS.pm
@@ -0,0 +1,5 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Acme::CPANModules::WorkingWithODS;
+use alias::module 'Acme::CPANModules::WorkingWithXLS';
+1;
+# ABSTRACT: Currently alias for Acme::CPANModules::WorkingWithXLS
---
+++ lib/Acme/CPANModules/WorkingWithXLSX.pm
@@ -0,0 +1,5 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Acme::CPANModules::WorkingWithXLSX;
+use alias::module 'Acme::CPANModules::WorkingWithXLS';
+1;
+# ABSTRACT: Currently alias for Acme::CPANModules::WorkingWithXLS
